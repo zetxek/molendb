8e9f5bd73c66678bee303c116047111a3ee996a3
Adrián Moreno Peña <zetxek@gmail.com>
2017-05-19 00:12:35 +0200

    Reorganized code a bit

--- download.go
+++ download.go
@@ -1,62 +1,8 @@
+// Downloads the files from molendb to xmls on disk
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"strings"
-)
+import "bitbucket.org/zetxek/molendb/files"
 
-func dlFile(url, filename string) {
-	fmt.Println("Downloading " + url + " ...")
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	f, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	io.Copy(f, resp.Body)
-}
-
-func Download() {
-
-	var base = "https://molendatabase.nl/nederland/bewaartypemarkers.php?type="
-	var types = [...]string{
-		"Beltmolen",
-		"Grondzeiler",
-		"Tjasker",
-		"Kleine molen",
-		"Paltrokmolen",
-		"Spinnenkop",
-		"Stellingmolen",
-		"Wipmolen",
-		"Weidemolen",
-		"Standerdmolen",
-		"Gesloten standerdmolen",
-		"Torenmolen",
-		"Tonmolen",
-		"Tredmolen",
-		"Achtkante molen",
-		"Zeskante molen",
-		"Achtkante binnenkruier",
-		"Watermolen"}
-
-	for _, element := range types {
-		var f = strings.ToLower(element)
-
-		f = strings.Replace(f, " ", "", -1)
-		f = fmt.Sprintf("%s.xml", f)
-		var url = fmt.Sprintf("%s%s", base, element)
-
-		dlFile(url, f)
-		fmt.Println(f + " saved!")
-	}
-
+func main() {
+	files.Download("./files/")
 }
--- main.go
+++ main.go
@@ -9,8 +9,6 @@ func main() {
 	d := db.OpenDB(db.GetDBName())
 	defer d.Close()
 
-	//Van bossestraat
-	db.ClosestMill(d, 52.3759129, 4.8643734)
-	// Jan Haringstraat
-	db.ClosestMill(d, 52.3760028, 4.8603556)
+	// Dam Square
+	db.ClosestMill(d, 52.3759976, 4.8264306)
 }
